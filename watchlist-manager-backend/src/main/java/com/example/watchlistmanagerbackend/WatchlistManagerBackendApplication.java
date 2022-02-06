a958a9855769b8fcb159ef9e20ff96cdb4f0c9ed
Marina Hornung <waggeling95@web.de>
2022-02-06 15:02:35 +0100

    Added backend

---
+++ watchlist-manager-backend/src/main/java/com/example/watchlistmanagerbackend/WatchlistManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.watchlistmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchlistManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchlistManagerBackendApplication.class, args);
+	}
+
+}
---
+++ watchlist-manager-backend/src/test/java/com/example/watchlistmanagerbackend/WatchlistManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watchlistmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
