2454c43c0818f97695f6ac4539e1d1fbb172acb7
Marina Hornung <waggeling95@web.de>
2022-02-10 17:58:55 +0100

    Added sorting of different table columns

--- watchlist-manager-frontend/src/components/ListWatchedSeriesComponent.js
+++ watchlist-manager-frontend/src/components/ListWatchedSeriesComponent.js
@@ -6,18 +6,20 @@ export default class ListWatchedSeriesComponent extends Component {
         super(props)
 
         this.state = {
-            watchedSeries: []
+            watchedSeries: [],
+            sortBy: ''
         }
         this.addWatchedSeries = this.addWatchedSeries.bind(this);
         this.updateWatchedSeries = this.updateWatchedSeries.bind(this);
         this.deleteWatchedSeries = this.deleteWatchedSeries.bind(this);
+        this.sortWatchedSeries = this.sortWatchedSeries.bind(this);
     }
   
     componentDidMount() {
         WatchedSeriesService.getWatchedSeries().then((res) => {
-            let data = res.data
+            let data = res.data;
             data.sort((a, b) => a.id - b.id);
-            this.setState({watchedSeries: data});
+            this.setState({watchedSeries: data, sortBy: 'id'});
         });
     }
 
@@ -33,6 +35,32 @@ export default class ListWatchedSeriesComponent extends Component {
         });
     }
 
+    sortWatchedSeries(sortBy) {
+        let sortedBy = this.state.sortBy;
+        let watchedSeries = this.state.watchedSeries;
+        if (sortBy === sortedBy) {
+            this.setState({watchedSeries: watchedSeries.reverse()});
+        } else {
+            switch(sortBy) {
+                case 'rating':
+                    watchedSeries.sort((a, b) => a.rating > b.rating);
+                    break;
+                case 'name':
+                    watchedSeries.sort((a, b) => a.name > b.name);
+                    break;
+                case 'yearWatched':
+                    watchedSeries.sort((a, b) => a.yearWatched > b.yearWatched);
+                    break;
+                case 'productionCountry':
+                    watchedSeries.sort((a, b) => a.productionCountry > b.productionCountry);
+                    break;
+                default: 
+                    break;
+            }
+            this.setState({watchedSeries: watchedSeries, sortBy: sortBy});
+        }
+    }
+
     updateWatchedSeries(id) {
         this.props.navigate(`/update-watchedseries/${id}`);
     }
@@ -44,7 +72,7 @@ export default class ListWatchedSeriesComponent extends Component {
     };
     const btnDelete = {
         marginLeft: "10px"
-    }
+    };
     return <div>
         <h2 className='text-center'>Watched Series List</h2>
         <div className='row'>
@@ -54,11 +82,11 @@ export default class ListWatchedSeriesComponent extends Component {
             <table className='table table-striped table-bordered'>
                 <thead>
                     <tr className='text-center'>
-                        <th>Rating</th>
-                        <th>Name</th>
+                        <th>Rating <button onClick={() => this.sortWatchedSeries('rating')} className="btn btn-outline-primary btn-bordered btn-sm float-end">sort</button></th>
+                        <th>Name <button onClick={() => this.sortWatchedSeries('name')} className="btn btn-outline-primary btn-sm float-end">sort</button></th>
                         <th>Genre</th>
-                        <th>Year Watched</th>
-                        <th>Production Country</th>
+                        <th>Year Watched <button onClick={() => this.sortWatchedSeries('yearWatched')} className="btn btn-outline-primary btn-sm float-end">sort</button></th>
+                        <th>Production Country <button onClick={() => this.sortWatchedSeries('productionCountry')} className="btn btn-outline-primary btn-sm float-end">sort</button></th>
                         <th>Actions</th>
                     </tr>
                 </thead>
